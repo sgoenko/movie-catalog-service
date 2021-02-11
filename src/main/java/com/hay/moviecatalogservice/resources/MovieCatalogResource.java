61abe0872c1f0a8e09d3f422a7a52f67d9132e5b
sgoenko <sgoenko@gmail.com>
2021-02-11 23:07:12 +0300

    Eureka client added

--- src/main/java/com/hay/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/hay/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
@@ -24,19 +24,18 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private WebClient.Builder webClientBuilder;
-
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         UserRating userRating = restTemplate.getForObject(
-                "http://localhost:8083/ratingsdata/users/" + userId,
-                UserRating.class);
+                "http://ratings-data-service/ratingsdata/users/" + userId,
+                UserRating.class
+        );
 
         return userRating.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-
+            Movie movie = restTemplate.getForObject(
+                    "http://movie-info-service/movies/" + rating.getMovieId(), Movie.class
+            );
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         })
         .collect(Collectors.toList());
