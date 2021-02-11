bdc8d6abd3c759841b31e003ccd0c955899e2920
sgoenko <sgoenko@gmail.com>
2021-02-11 14:00:32 +0300

    Create RestTemplate bean

--- src/main/java/com/hay/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/hay/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.hay.moviecatalogservice.resources;
 import com.hay.moviecatalogservice.models.CatalogItem;
 import com.hay.moviecatalogservice.models.Movie;
 import com.hay.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,11 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
-        RestTemplate restTemplate = new RestTemplate();
 
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234", 4),
