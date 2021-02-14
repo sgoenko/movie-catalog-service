e1586d653e5aa28c8942f85d4afc34df031cecff
sgoenko <sgoenko@gmail.com>
2021-02-14 15:09:57 +0300

    Hystrix added

--- src/main/java/com/hay/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/hay/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.hay.moviecatalogservice.resources;
 import com.hay.moviecatalogservice.models.CatalogItem;
 import com.hay.moviecatalogservice.models.Movie;
 import com.hay.moviecatalogservice.models.UserRating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,6 +26,7 @@ public class MovieCatalogResource {
     private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         UserRating userRating = restTemplate.getForObject(
@@ -42,6 +45,10 @@ public class MovieCatalogResource {
 
     }
 
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+        return Arrays.asList(new CatalogItem("No movie", "no", 0));
+
+    }
 }
 
 //            Movie movie = webClientBuilder.build()
